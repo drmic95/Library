e933bf3777421473b029a74c1050a7176e68b460
drmic95 <ante.drmic1b@gmail.com>
2022-09-11 19:09:03 +0200

    js logic done mostly done, CF deleted

--- script.js
+++ script.js
@@ -1,66 +1,60 @@
 'strict mode';
 
-let objectLibrary = [];
-let arrayLibrary = [];
-
 const addBooks = document.querySelector('.add_books');
 const booksContainer = document.querySelector('.books_container');
 
-// CF for new book objects
-function CFbook(title, author, genre) {
-  this.title = title;
-  this.author = author;
-  this.genre = genre;
-}
-
 function addBookToLibrary() {
-  // input variables
-  const t = prompt('title');
-  const a = prompt('author');
-  const g = prompt('genre');
-
-  // creating new objects
-  objectLibrary.push(new CFbook(t, a, g));
-
-  // object into array at any new given position
-  arrayLibrary.push(Object.entries(objectLibrary[objectLibrary.length - 1]));
-
-  console.log(arrayLibrary);
-  console.log(objectLibrary);
   const parentElement = document.createElement('div');
   booksContainer.appendChild(parentElement);
   parentElement.classList.add('parentElement');
-  const childElement1 = document.createElement('li');
-  const childElement2 = document.createElement('li');
-  const childElement3 = document.createElement('li');
+
+  const author = document.createElement('section');
+  const childElement1 = document.createElement('input');
+  const title = document.createElement('section');
+  const childElement2 = document.createElement('input');
+  const genre = document.createElement('section');
+  const childElement3 = document.createElement('input');
+
+  childElement1.setAttribute('type', 'text');
+  childElement2.setAttribute('type', 'text');
+  childElement3.setAttribute('type', 'text');
+  childElement1.setAttribute('size', '10');
+  childElement2.setAttribute('size', '10');
+  childElement3.setAttribute('size', '10');
+  childElement1.setAttribute('id', 'title');
+  childElement2.setAttribute('id', 'book');
+  childElement3.setAttribute('id', 'genre');
+
+  author.textContent = 'Author';
+  title.textContent = 'Title';
+  genre.textContent = 'Genre';
+
+  parentElement.appendChild(author);
   parentElement.appendChild(childElement1);
+  parentElement.appendChild(title);
   parentElement.appendChild(childElement2);
+  parentElement.appendChild(genre);
   parentElement.appendChild(childElement3);
 
-  for (let i = 0; i < arrayLibrary.length; i++) {
-    childElement1.textContent = `${arrayLibrary[i][0][0]}: ${arrayLibrary[i][0][1]}`;
-    childElement2.textContent = `${arrayLibrary[i][1][0]}: ${arrayLibrary[i][1][1]}`;
-    childElement3.textContent = `${arrayLibrary[i][2][0]}: ${arrayLibrary[i][2][1]}`;
-  }
   const divChildren = document.querySelectorAll('div');
   for (i = 0; i < divChildren.length; i++) {
     divChildren[i].classList.add(`book_${i + 1}`);
   }
-  const editBtn = document.createElement('button');
+  const readBtn = document.createElement('button');
   const deleteBtn = document.createElement('button');
-  parentElement.appendChild(editBtn);
+
+  parentElement.appendChild(readBtn);
   parentElement.appendChild(deleteBtn);
-  editBtn.textContent = 'edit';
+  readBtn.textContent = 'unread';
+  readBtn.classList.add('read');
   deleteBtn.textContent = 'delete';
-  editBtn.classList.add('edit');
   deleteBtn.classList.add('delete');
 
-  const allEditBtn = document.querySelectorAll('.edit');
   const allDeleteBtn = document.querySelectorAll('.delete');
+  const allReadBtn = document.querySelectorAll('.read');
 
   // assign classes to btn upon creation _1
   for (i = 0; i < divChildren.length; ++i) {
-    allEditBtn[i].classList.add(`edit_${i + 1}`);
     allDeleteBtn[i].classList.add(`delete_${i + 1}`);
   }
   // delete listener
@@ -70,8 +64,17 @@ function addBookToLibrary() {
     });
   });
 
-  // edit listener
+  allReadBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.target.classList.add('visited');
+      e.target.textContent = 'read';
+    });
+  });
 }
 addBooks.addEventListener('click', addBookToLibrary);
 
 // add eventListener for every delete/edit btn with loop, either deleting index numbers or elements directly
+
+// input instead of section; no need for prompts and edit btn is not needed as well
+
+// js work is mostly done, maybe add read checkbox later; now css is in order
